3fddd80999889d62d39a04e071f8a88097db5954
A. Ribeiro dos Santos <a.ribeiro.dos.santos@avanade.com>
2019-11-12 18:01:37 -0300

    set users components

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,14 @@ import { Router } from 'express';
 
 const routes = new Router();
 
+import authController from './app/components/auth/authController';
+
 routes.get('/', (req, res) => res.json({ message: 'ok' }));
+
+router.route('/login')
+  .post(authController.login)
+
+router.route('/signup')
+  .post(authController.signup)
 
 export default routes;
